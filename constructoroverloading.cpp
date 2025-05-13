1914f5ebfca478b0ecf1204dad2db2f61dcca747
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-05-13 19:17:32 +0700

    output nama dan nim mahasiswa (constructor overloading)

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -25,3 +25,26 @@ mahasiswa::mahasiswa(int iNim){
 mahasiswa::mahasiswa(string iNama){
     nama = iNama;
 }
+
+mahasiswa::mahasiswa(int iNim, string iNama){
+    nim = iNim;
+    nama = iNama;
+}
+
+void mahasiswa::cetak(){
+
+    cout << endl << "Nim = " << nim <<endl;
+    cout << "Nama = " << nama <<endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030);
+    mahasiswa mhs2("kyy");
+    mahasiswa mhs3(102003, "Mas");
+
+    cout << "mahasiswa 1:"; mhs1.cetak();
+    cout << "mahasiswa 2:"; mhs2.cetak();
+    cout << "mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
