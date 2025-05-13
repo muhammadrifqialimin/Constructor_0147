3e8aaebc51232effc29894e83770aec13ae53347
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-05-13 18:55:32 +0700

    memanggil constructor berparameter, nama dan nim

--- constructorBerparameter.cpp
+++ constructorBerparameter.cpp
@@ -6,3 +6,9 @@ class mahasiswa {
 public:
 mahasiswa(int nim, string nama);
 };
+
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constructor dengan parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
