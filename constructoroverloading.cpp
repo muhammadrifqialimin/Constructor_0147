4bbd7e392e79a346b6e46b807dd1989c72a0eb0b
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-05-13 19:06:45 +0700

    definisi hanya Nim

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -12,5 +12,12 @@ public:
     mahasiswa(string);
     mahasiswa (int iNim, string iNama);
     void cetak ();
-    
+
 };
+
+mahasiswa::mahasiswa(){
+}
+
+mahasiswa::mahasiswa(int iNim){
+    nim = iNim;
+}
